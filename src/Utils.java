c033d5f4c3bb6f13a0fccc5aab874d11d83401d6
enrique-AMT <enrique.marrero1@upr.edu>
2017-02-23 21:36:28 -0400

    Started adding javadoc entries, general cleanup

--- src/Utils.java
+++ src/Utils.java
@@ -1,8 +1,9 @@
-import java.io.IOException;
-import java.io.RandomAccessFile;
-
 public class Utils{
-
+	/**
+	 * Verifies if the block size is a power of 2.
+	 * @param blockSize the block size to check.
+	 * @return a boolean value representing if the block size is correct.
+	 */
 	public static boolean powerOf2(int blockSize) {
 		
 		return (blockSize%2==0);
--- src/VirtualDiskBlock.java
+++ src/VirtualDiskBlock.java
@@ -5,6 +5,9 @@ import java.io.RandomAccessFile;
 public class VirtualDiskBlock {
 	
 	private RandomAccessFile diskBlock;
+	/**
+	 * Creates a Virtual Disk Block with the default capacity of 256 bytes.
+	 */
 	public VirtualDiskBlock(){
 		 try {
 	         diskBlock = new RandomAccessFile("OriginalBlock", "rw");
@@ -15,6 +18,11 @@ public class VirtualDiskBlock {
 	     }
 		
 	}
+	/**
+	 * Creates a Virtual Disk Block with a specified block capacity
+	 * @param blockCapacity the capacity (multiplier of the original 256 bytes) to append
+	 * to the disk block.
+	 */
 	public VirtualDiskBlock(int blockCapacity){
 		try {
 			diskBlock = new RandomAccessFile("ModifiedBlock", "rw");
@@ -27,6 +35,12 @@ public class VirtualDiskBlock {
 		
 	}
 	
+	/**
+	 * Method to obtain the capacity of the disk block in question.
+	 * @return a non-negative integer which represents the amount of bytes the
+	 * disk block contains.
+	 */
+	
 	public int getCapacity(){
 		int capacity=0;
 		try {
@@ -38,6 +52,13 @@ public class VirtualDiskBlock {
 			return capacity;
 		
 	}
+	
+	/**
+	 * Sets the byte in the selected index of the disk block
+	 * @param index the disk block's index of memory, converted to long.
+	 * @param the byte to write in the index of the Virtual Disk Block.
+	 */
+	
 	public void setElement(int index, byte nuevo){
 		try {
 			diskBlock.seek((long) index);
@@ -48,6 +69,13 @@ public class VirtualDiskBlock {
 		}
 		
 	}
+	
+	/**
+	 * gets the element in a specified index
+	 * @param index the index to search inside the Virtual Disk Block.
+	 * @return the element contained in the specified index.
+	 */
+	
 	public byte getElement(int index){
 		byte element=0;
 		try {
